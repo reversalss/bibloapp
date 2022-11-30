586796960819465820e5608daa3122fa02787d02
reversalss <wwoinfinityx@gmail.com>
2022-11-30 22:07:48 +0100

    Support for adding books as an admin user

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ except ImportError:
     print("Can't import modules")
 
 
+# COLORS
 
 mainBlue = "#04386b"
 secBlue = "#11589e"
@@ -62,8 +63,78 @@ def appScreen(username, studentID):
     return_button = Button(appS, text="Return Book", font=('JetBrains mono', 22), bg="#11589e", fg="white", relief="flat", width="15")
     return_button.pack(pady=(15, 0))
 
+
+    def bookadd():
+
+    
+        addB = Tk()
+        addB.geometry("400x500")
+        addB.iconbitmap("logo.ico")
+        addB.title("BibloApp")
+        addB.config(background="#04386b")
+        addB.resizable(False, False)
+
+        # Widgets
+
+        header = Label(addB, text="Register")
+        header.config(font=('Akira Expanded', 40), bg="#04386b", fg="white")
+        header.pack(pady=25)
+
+
+        # bookname
+
+        label_bookname = Label(addB, text="Book Name:", bg=mainBlue, fg="white", font=("JetBrains Mono", 10))
+        label_bookname.pack(pady=5)
+
+        bookname_entry = Entry(addB, fg="white", bg=secBlue, width=15, font=("Arial", 25))
+        bookname_entry.pack(pady=5)
+
+
+        # ISBN
+
+        label_ISBN = Label(addB, text="ISBN:", bg=mainBlue, fg="white", font=("JetBrains Mono", 10))
+        label_ISBN.pack(pady=5)
+
+        ISBN_entry = Entry(addB, fg="white", bg=secBlue, width=15, font=("Arial", 25))
+        ISBN_entry.pack(pady=5)
+
+
+
+        # SUBMIT
+
+        def addbook():
+
+            print("adding book")
+
+            # get info
+
+            bookname_var = bookname_entry.get()
+            ISBN_var = ISBN_entry.get()
+
+            # check if ISBN already exists
+
+            f = open("books.txt", "r")
+            for i in f.readlines():
+                if i.split("|")[0] == ISBN_var:
+                    popup("ISBN allerede lagt til")
+                    f.close()
+                    break
+            else:
+                # if it doesn't, make a new book with new info
+                f = open("books.txt", "a")
+                f.write(f"\n{ISBN_var}:{bookname_var}")
+                popup("Adding book complete! You can now close this window.")
+                f.close()
+
+                
+        submit_button = Button(addB, text="Add Book", font=('JetBrains mono', 15), bg="#11589e", fg="white", relief="flat", width="15")
+        submit_button.config(command=addbook)
+        submit_button.pack(pady=25)
+
+
+
     if int(studentID) == 1:
-        add_book = Button(appS, text="Add Book", font=('JetBrains mono', 22), bg="#11589e", fg="white", relief="flat", width="15")
+        add_book = Button(appS, text="Add Book", font=('JetBrains mono', 22), bg="#11589e", fg="white", relief="flat", width="15", command=bookadd)
         add_book.pack(pady=(50, 0))
 
 def login():
